7a268e0dd39773b8704c207718c5bdb4dd11b9aa
thehawkseye <benjaminpporter@gmail.com>
2026-04-19 22:47:11 -0500

    Commjit

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -7,22 +7,23 @@ exports.handler = async function (event) {
         return { statusCode: 400, body: 'Missing url parameter' };
     }
 
-    return new Promise((resolve) => {
-        https.get(targetUrl, (res) => {
-            let data = '';
-            res.on('data', chunk => data += chunk);
-            res.on('end', () => {
-                resolve({
+    function fetchUrl(url, redirects = 5) {
+        return new Promise((resolve, reject) => {
+            https.get(url, (res) => {
+                if ((res.statusCode === 301 || res.statusCode === 302) && res.headers.location && redirects > 0) {
+                    return resolve(fetchUrl(res.headers.location, redirects - 1));
+                }
+                let data = '';
+                res.on('data', chunk => data += chunk);
+                res.on('end', () => resolve({
                     statusCode: 200,
-                    headers: {
-                        'Access-Control-Allow-Origin': '*',
-                        'Content-Type': 'application/json'
-                    },
+                    headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
                     body: data
-                });
-            });
-        }).on('error', (err) => {
-            resolve({ statusCode: 500, body: err.message });
+                }));
+            }).on('error', reject);
         });
-    });
+    }
+
+    try { return await fetchUrl(targetUrl); }
+    catch (err) { return { statusCode: 500, body: err.message }; }
 };
